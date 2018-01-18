e169f08779d825fa854fd0bf763ddaa0c1ab2d1c
四川三君科技有限公司 <www.448128835@qq.com>
2018-01-18 09:43:07 +0800

    no message

--- excellentPurchase/excellentHeader.h
+++ excellentPurchase/excellentHeader.h
@@ -11,37 +11,6 @@
 
 #define YYSRGBColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
 
-/**
- 首页banner图片比例
- */
-#define bannerScale   459.0 / 750
-
-/**
- 首页banner cell的高度
- */
-#define bannerHeiget   60
-
-/**
- 首页底部图片比例
- */
-#define bottomScale   240 / 750.0
-
-/**
-吃喝玩乐-精品推荐图片比例
- */
-#define EatrecommendScle   93 / 113.0
-
-/**
- 吃喝玩乐-cell的高度
- */
-#define EatCellH   120.0
-
-/**宽度比例*/
-#define CZH_ScaleWidth(__VA_ARGS__)  ([UIScreen mainScreen].bounds.size.width/375)*(__VA_ARGS__)
-
-/**高度比例*/
-#define CZH_ScaleHeight(__VA_ARGS__)  ([UIScreen mainScreen].bounds.size.height/667)*(__VA_ARGS__)
-
 /**占位图*/
 #define PlaceHolder @"shijiyougou"
 
--- excellentPurchase/scaleHeader.h
+++ excellentPurchase/scaleHeader.h
@@ -9,5 +9,36 @@
 #ifndef scaleHeader_h
 #define scaleHeader_h
 
+/**
+ 首页banner图片比例
+ */
+#define bannerScale   459.0 / 750
+
+/**
+ 首页banner cell的高度
+ */
+#define bannerHeiget   60
+
+/**
+ 首页底部图片比例
+ */
+#define bottomScale   240 / 750.0
+
+/**
+ 吃喝玩乐-精品推荐图片比例
+ */
+#define EatrecommendScle   93 / 113.0
+
+/**
+ 吃喝玩乐-cell的高度
+ */
+#define EatCellH   120.0
+
+/**宽度比例*/
+#define CZH_ScaleWidth(__VA_ARGS__)  ([UIScreen mainScreen].bounds.size.width/375)*(__VA_ARGS__)
+
+/**高度比例*/
+#define CZH_ScaleHeight(__VA_ARGS__)  ([UIScreen mainScreen].bounds.size.height/667)*(__VA_ARGS__)
+
 
 #endif /* scaleHeader_h */
